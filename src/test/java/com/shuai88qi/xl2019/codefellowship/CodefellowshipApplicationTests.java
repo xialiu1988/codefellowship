039430f7de82a19a52afdc27d690ac24484a0e5d
xialiu1988 <liuxia198896@gmail.com>
2019-06-17 19:32:16 -0700

    first commit

---
+++ src/test/java/com/shuai88qi/xl2019/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shuai88qi.xl2019.codefellowship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodefellowshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
